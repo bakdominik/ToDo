24c854d2249ac9dc79101481ac48c320312fe428
bakdominik <dominikbak@gmail.com>
2020-02-22 14:40:42 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- todo/settings.py
+++ todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'mz8_valsy5uryotcbgigwib&*aiwki
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://powerful-hollows-40233.herokuapp.com','127.0.0.1']
 
 
 # Application definition
