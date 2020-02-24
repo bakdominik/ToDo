abc311487317444b2c68ccd3720b69dc852d2f43
bakdominik <dominikbak@gmail.com>
2020-02-24 15:09:13 +0100

    added show all tasks function

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('tasks', views.tasks, name='tasks'),
     path('delete_task', views.delete_task, name='delete_task'),
     path('mark_as_done', views.mark_as_done, name='mark_as_done'),
+    path('show_all_tasks', views.show_all_tasks, name='show_all_tasks'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -20,18 +20,23 @@ def tasks(request):
             task.date = request.POST['date']
             task.user = request.user
             task.save()
+            if request.POST['all']:
+                return redirect('show_all_tasks')
             return redirect('tasks')
         else:
-            return render(request, 'todoapp/tasks.html', {'error':'All fields are required'})
+            tasks = Task.objects.filter(user=request.user,date=date.today())
+            return render(request, 'todoapp/tasks.html', {'show':'Today','error':'All fields are required','tasks':tasks,'date':date.today().isoformat()})
     else:
         tasks = Task.objects.filter(user=request.user,date=date.today())
-        return render(request, 'todoapp/tasks.html', {'tasks':tasks})
+        return render(request, 'todoapp/tasks.html', {'show':'Today','tasks':tasks,'date':date.today().isoformat()})
 
 @login_required()
 def delete_task(request):
     if request.method == 'POST':
         task = Task.objects.get(pk=request.POST['id'])
         task.delete()
+        if request.POST['all']:
+            return redirect('show_all_tasks')
         return redirect('tasks')
 
 @login_required()
@@ -45,4 +50,19 @@ def mark_as_done(request):
         task.save()
     return redirect('tasks')
 
-        
+@login_required()
+def show_all_tasks(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['date']:
+            task = Task()
+            task.title = request.POST['title']
+            task.date = request.POST['date']
+            task.user = request.user
+            task.save()
+            return redirect('tasks')
+        else:
+            tasks = Task.objects.filter(user=request.user,date=date.today())
+            return render(request, 'todoapp/tasks.html', {'show':'All tasks','error':'All fields are required','tasks':tasks,'date':date.today().isoformat()})
+    else:
+        tasks = Task.objects.filter(user=request.user)
+        return render(request, 'todoapp/tasks.html', {'show':'All tasks','all':True,'tasks':tasks,'date':date.today().isoformat()})
